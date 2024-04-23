057952c6beea3ca2efab5fc8bf89136937e00b02
Elvis Saavedra <elvis.saavedra@nearsure.com>
2024-04-23 09:16:25 -0500

    Add TrelloBoard, TrelloBoardTask components and update app.vue template

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="p-10 h-[100vh] bg-teal-600 overflow-auto">
+    <h1 class="text-4xl text-white font-bold flex items-center mb-10">
+      <img
+        src="https://vueschool.io/img/logo/vueschool_logo_multicolor_negative.svg"
+        width="200"
+        alt="Vue School Logo"
+        class="mr-3"
+      />
+      Trello Board
+    </h1>
+    <TrelloBoard />
   </div>
 </template>
---
+++ components/TrelloBoard.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="flex gap-4 overflow-x-auto items-start">
+    <div
+      v-for="column in columns"
+      :key="column.id"
+      class="column bg-gray-200 p-5 rounded min-w-[250px]"
+    >
+      <header class="font-bold mb-4">
+        {{ column.title }}
+      </header>
+      <TrelloBoardTask
+        v-for="task in column.tasks"
+        :key="task.id"
+        :task="task"
+      />
+      <footer>
+        <button class="text-gray-500">+ Add a Card</button>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Column } from '@/types'
+import { nanoid } from 'nanoid'
+
+const columns = reactive<Column[]>([
+  {
+    id: nanoid(),
+    title: 'To do',
+    tasks: [
+      { id: nanoid(), title: 'Task 1', createdAt: new Date() },
+      { id: nanoid(), title: 'Task 2', createdAt: new Date() },
+    ],
+  },
+  {
+    id: nanoid(),
+    title: 'In progress',
+    tasks: [],
+  },
+  {
+    id: nanoid(),
+    title: 'Review',
+    tasks: [],
+  },
+  {
+    id: nanoid(),
+    title: 'Ready for QA',
+    tasks: [],
+  },
+  {
+    id: nanoid(),
+    title: 'QA Finished',
+    tasks: [],
+  },
+  {
+    id: nanoid(),
+    title: 'Done',
+    tasks: [],
+  },
+])
+</script>
