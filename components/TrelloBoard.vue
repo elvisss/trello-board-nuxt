7111db320c9ccb01b4b2a4fbbe91fb3a1f41fadb
Elvis Saavedra <elvis.saavedra@nearsure.com>
2024-04-23 10:40:43 -0500

    added draggable

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -1,28 +1,35 @@
 <template>
-  <div class="flex gap-4 overflow-x-auto items-start">
-    <div
-      v-for="column in columns"
-      :key="column.id"
-      class="column bg-gray-200 p-5 rounded min-w-[250px]"
-    >
-      <header class="font-bold mb-4">
-        {{ column.title }}
-      </header>
-      <TrelloBoardTask
-        v-for="task in column.tasks"
-        :key="task.id"
-        :task="task"
-      />
-      <footer>
-        <button class="text-gray-500">+ Add a Card</button>
-      </footer>
-    </div>
-  </div>
+  <draggable
+    :list="columns"
+    group="columns"
+    item-key="id"
+    class="flex gap-4 overflow-x-auto items-start"
+  >
+    <template #item="{ element: column }: { element: Column }">
+      <div
+        class="column bg-gray-200 p-5 rounded min-w-[250px]"
+      >
+        <header class="font-bold mb-4">
+          {{ column.title }}
+        </header>
+        <TrelloBoardTask
+          v-for="task in column.tasks"
+          :key="task.id"
+          :task="task"
+        />
+        <footer>
+          <button class="text-gray-500">+ Add a Card</button>
+        </footer>
+      </div>
+    </template>
+  </draggable>
 </template>
 
 <script setup lang="ts">
-import type { Column } from '@/types'
 import { nanoid } from 'nanoid'
+import draggable from 'vuedraggable'
+
+import type { Column } from '@/types'
 
 const columns = reactive<Column[]>([
   {
